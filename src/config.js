979b4e99458b136b35b1e527a1f81a530b9e4bb3
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-07-11 21:22:31 -0700

    changed config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable strict */
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB_URL: process.env.DATABASE_URL,
+  DB_URL: process.env.DATABASE_URL || 'postgresql://flavors@localhost/flavors',
   NODE_ENV: process.env.NODE_ENV || 'developement',
 }
